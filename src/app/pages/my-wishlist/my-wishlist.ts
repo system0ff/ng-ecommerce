fcb2536f9cc937a645888e2a3578f6afe2930b05
Daniel Rodrigues de Santana <daniel.desantana@castgroup.com.br>
2026-01-14 16:19:30 -0300

    09 - added style to remove icon (wishlist page)

--- src/app/pages/my-wishlist/my-wishlist.ts
+++ src/app/pages/my-wishlist/my-wishlist.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { BackButton } from '../../components/back-button/back-button';
 import { EcommerceStore } from '../../stores/ecommerce-store';
 import { ProductCard } from '../../components/product-card/product-card';
-import { ToggleWishlistButton } from '../../components/toggle-wishlist-button/toggle-wishlist-button';
+import { MatIcon } from '@angular/material/icon';
+import { MatIconButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-my-wishlist',
-  imports: [BackButton, ProductCard, ToggleWishlistButton],
+  imports: [BackButton, ProductCard, MatIconButton, MatIcon],
   template: `
     <div class="mx-auto max-w-[1200] py-6 px-4">
       <app-back-button class="mb-6" label="Continue Shopping" navigateTo="/products/all">
@@ -22,7 +23,13 @@ import { ToggleWishlistButton } from '../../components/toggle-wishlist-button/to
       <div class="responsive-grid">
         @for (product of store.wishlistItems(); track product.id) {
         <app-product-card [product]="product">
-          <app-toggle-wishlist-button [product]="product" />
+          <button
+            matIconButton
+            class="!absolute z-10 top-3 right-3 w-10 rounded-full !bg-white border-0 shadow-md flex items-center justify-center cursor-pointer transition-all duration-200 hover:scale-110 hover:shadow-lg"
+            (click)="store.removeFromWishlist(product)"
+          >
+            <mat-icon>delete</mat-icon>
+          </button>
         </app-product-card>
         }
       </div>
